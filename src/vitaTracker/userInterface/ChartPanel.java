18d6850df974d21956d053cff483cfaf748c48af
benjamin.dux <benjamin.dux@googlemail.com>
2014-10-13 15:16:14 +0200

    added JTable

--- src/vitaTracker/userInterface/ChartPanel.java
+++ src/vitaTracker/userInterface/ChartPanel.java
@@ -1,15 +1,25 @@
 package vitaTracker.userInterface;
 
+import java.awt.Rectangle;
+
 import javax.swing.JPanel;
+import javax.swing.JTable;
+
 import org.jfree.chart.*;
 
 
 public class ChartPanel extends JPanel
 {
 
+	
+	private JTable table = null;
+
+	
+	
 	public ChartPanel()
 	{
 		
+		table = new JTable();
 		
 		
 	}
